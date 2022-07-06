7823cc54d363eed64272825410d858cd716edd97
Gracilliano Carvalho <gracillianowp@hotmail.com>
2022-07-06 18:56:25 -0300

    Listando informações fornecidas pela API

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,46 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { api } from 'boot/axios'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+
+    const columns = [
+      { name: 'id', field: 'id', label: 'ID', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'Título', align: 'left' },
+      { name: 'author', field: 'author', label: 'Autor', align: 'left' }
+    ]
+
+    const getPosts = async () => {
+      try {
+        const { data } = await api.get('posts')
+        posts.value = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    return {
+      posts,
+      columns
+    }
+  }
 })
 </script>
